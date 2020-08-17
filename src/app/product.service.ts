a4b3de2ab0b951cf17ef52349e97b31786495618
elmassafi <anas.elmassafi@gmail.com>
2020-08-17 10:00:25 +0100

    Adding the Tracklisting Component

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,10 +1,11 @@
-// tslint:disable-next-line:comment-format
-//import { Injectable } from '@angular/core';
+
 import {Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
 import {Observable} from 'rxjs/Observable';
 import {Album} from './album';
-/*@Injectable()*/
+import {Injectable} from '@angular/core';
+
+@Injectable()
 export class ProductService {
 
   constructor(private _http: Http) { }
